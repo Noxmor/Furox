04367be4c8986b03422df2291e0969b94a7bef86
Noxmor <lord.noxmor@gmail.com>
2023-12-17 16:12:46 +0100

    Added types.h

---
+++ bootstrap/src/core/types.h
@@ -0,0 +1,29 @@
+#ifndef FRX_TYPES_H
+#define FRX_TYPES_H
+
+#include <stdint.h>
+
+#define FRX_FALSE 0
+#define FRX_TRUE 1
+
+typedef size_t usize;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef u8 b8;
+typedef u16 b16;
+typedef u32 b32;
+typedef u64 b64;
+
+typedef float f32;
+typedef double f64;
+
+#endif
