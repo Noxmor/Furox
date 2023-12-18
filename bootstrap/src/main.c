b060350fd0c44dfc406c9cd4a8f9531dce79ef86
Noxmor <lord.noxmor@gmail.com>
2023-12-18 01:43:22 +0100

    Tested memory tracer

--- bootstrap/src/main.c
+++ bootstrap/src/main.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+#include "core/memory.h"
+
 int main(void)
 {
-    printf("Hello World!\n");
+    memory_print();
+
+    void* mem0 = memory_alloc(512, FRX_MEMORY_CATEGORY_UNKNOWN);
+
+    void* mem1 = memory_alloc(256, FRX_MEMORY_CATEGORY_STRING);
+    
+    void* mem2 = memory_alloc(32, FRX_MEMORY_CATEGORY_UNKNOWN);
+
+    memory_print();
+
+    memory_free(mem0);
+    memory_free(mem1);
+    
+    memory_print();
+
+    mem2 = memory_realloc(mem2, 64, FRX_MEMORY_CATEGORY_STRING);
+
+    memory_print();
 
     return 0;
 }
