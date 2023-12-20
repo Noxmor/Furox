87b76e130bea768d6aa6b23056c8d23ed7922697
Noxmor <lord.noxmor@gmail.com>
2023-12-20 15:29:27 +0100

    Reduced compiler warnings
    Changed specifier from %zu to %d for __LINE__ to remove warnings.

--- bootstrap/src/core/assert.h
+++ bootstrap/src/core/assert.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 #ifdef FRX_ENABLE_ASSERTS
-#define FRX_ASSERT(condition) if(!(condition)) { fprintf(stderr, "Assertion failed at %s:%zu: \"%s\"\n", __FILE__, __LINE__, #condition); exit(EXIT_FAILURE); }
+#define FRX_ASSERT(condition) if(!(condition)) { fprintf(stderr, "Assertion failed at %s:%d: \"%s\"\n", __FILE__, __LINE__, #condition); exit(EXIT_FAILURE); }
 #else
 #define FRX_ASSERT(condition)
 #endif
