88b0cadf531dd7e7d27b48d8317b740a5d1e914f
Noxmor <lord.noxmor@gmail.com>
2025-01-30 18:23:35 +0100

    Bugfix: FRX_ASSERT() would always call exit()

--- compiler/include/assert.h
+++ compiler/include/assert.h
@@ -4,8 +4,8 @@
 #ifdef FRX_ENABLE_ASSERTS
 #include <stdlib.h>
 #include <stdio.h>
-#define FRX_ASSERT(condition) do { if (!(condition)) fprintf(stderr, "%s:%d:Assertion failed: %s\n", __FILE__, __LINE__, #condition);\
-    exit(EXIT_FAILURE); } while(0)
+#define FRX_ASSERT(condition) do { if (!(condition)) { fprintf(stderr, "%s:%d:Assertion failed: %s\n", __FILE__, __LINE__, #condition);\
+    exit(EXIT_FAILURE); } } while(0)
 #else
 #define FRX_ASSERT(condition)
 #endif
