d4e04fcf1ae43b2ff483d43a36b2943468265fe1
Noxmor <lord.noxmor@gmail.com>
2025-01-31 17:38:36 +0100

    Bugfix: Add modulo operation to correctly convert hash to index

--- compiler/src/string_table.c
+++ compiler/src/string_table.c
@@ -25,7 +25,7 @@ const char* string_table_intern(const char* str)
 {
     FRX_ASSERT(str != NULL);
 
-    u64 index = hash_djb2(str);
+    u64 index = hash_djb2(str) % FRX_STRING_TABLE_CAPACITY;
     StringTableEntry* entry = table.entries[index];
 
     while(entry != NULL)
