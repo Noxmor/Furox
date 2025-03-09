c769db4c4c827c5bd277be2d103bbe78caf1d608
Noxmor <lord.noxmor@gmail.com>
2025-03-09 17:32:57 +0100

    Fix permissions for commands in install script

--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    exec sudo "$0" "$@"
-fi
-
 echo "Compiling furoxc..."
 cd compiler
 make clean
 make
-
-echo "Installing furoxc..."
-cp ./bin/debug/furoxc /usr/local/bin/
 cd ..
 
-mkdir -p /usr/local/lib/furox/
-rm -rf /usr/local/lib/furox/stdlib/
+echo "Installing furoxc..."
+sudo cp ./compiler/bin/release/furoxc /usr/local/bin/
+
+sudo mkdir -p /usr/local/lib/furox/
+sudo rm -rf /usr/local/lib/furox/stdlib/
 echo "Installing stdlib..."
-cp -r ./stdlib/ /usr/local/lib/furox/
+sudo cp -r ./stdlib/ /usr/local/lib/furox/
 
 if [ ! -e "/usr/local/lib/furox/std" ]; then
-    ln -s /usr/local/lib/furox/stdlib/ /usr/local/lib/furox/std
+    sudo ln -s /usr/local/lib/furox/stdlib/ /usr/local/lib/furox/std
 fi
 
 echo "Successfully installed furoxc and stdlib"
