338272555123ed06b8c668da0797b032301a315d
Noxmor <lord.noxmor@gmail.com>
2026-03-25 16:07:35 +0100

    Handle postfix increment/decrement explicitly

--- bootstrap/src/codegen.c
+++ bootstrap/src/codegen.c
@@ -576,22 +576,19 @@ static void emit_unary_expr(AST* ast, FILE* f, CodegenContext* ctx)
 
     ASTUnaryExpr* unary_expr = &ast->unary_expr;
 
-    if (token_type_is_prefix_operator(unary_expr->type))
+    if (unary_expr->operator != FRX_OPERATOR_POST_INC
+        && unary_expr->operator != FRX_OPERATOR_POST_DEC)
     {
         fprintf(f, "(%s", token_type_to_str(unary_expr->type));
         emit_ast(unary_expr->operand, f, ctx);
         fprintf(f, ")");
     }
-    else if (token_type_is_postfix_operator(unary_expr->type))
+    else
     {
         fprintf(f, "(");
         emit_ast(unary_expr->operand, f, ctx);
         fprintf(f, "%s)", token_type_to_str(unary_expr->type));
 
-    }
-    else
-    {
-        FRX_ASSERT(FRX_FALSE);
     }
 }
 
