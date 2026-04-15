3107dda4e13b2606abf9659d5e3785254e03798c
Noxmor <lord.noxmor@gmail.com>
2026-04-15 22:05:10 +0200

    Rewrite test runner and update tests accordingly

--- bootstrap/tests.py
+++ bootstrap/tests.py
@@ -1,106 +1,247 @@
+#!/usr/bin/env python3
+
 """
 This script is designed to run tests for the Furox compiler.
-Every test is part of one of two categories: 'fail' or 'pass'.
 
-Category 'fail': A test that is meant to fail during compilation time.
-Categiry 'pass': A test that is meant to be compiled *and* executed successfully.
+HOW TO CREATE A NEW TEST:
+If your test consist only of one source file:
+Create a new source file in the {TESTS_DIR} directory. This will be the entry
+point for the test runner.
 
-Note: A successfull compilation/execution is indicated by an exit status of 0.
+If your test consists of multiple source files:
+Create a new directory in the {TESTS_DIR} directory. Inside that newly created
+directory create a file named 'main.frx'. This will be the entry point for the
+test runner script. You may freely add additional source files in this directory
+or any subdirectory. The test runner will automatically discover those
+source files.
 
-HOW TO CREATE A TEST:
-Tests are defined in the tests directory inside the subdirectory named after their category.
-Every project inside that subdirectory is treated as a test.
+All tests are expected to compile successfully and when executed return an exit
+code of 0.
 """
 
-import os
+import argparse
+from colorama import init, Fore, Style
+from dataclasses import dataclass
+from datetime import timedelta
+from enum import Enum, auto
+from pathlib import Path
 import subprocess
 import tempfile
-from colorama import init, Fore, Style
+from typing import Optional
 
-# Initializes colorama for colored output
-init()
+COMPILER = "./bin/debug/furoxc"
+TESTS_DIR = "tests"
 
-COMPILER = "./bin/debug/furoxc" # Path to the compiler this script uses
-TEST_DIR = "./tests" # Path to the tests directory
+MAX_TEST_NAME_LEN = 0
 
-PASS_COUNT = 0 # Number of passed tests
-FAIL_COUNT = 0 # Number of failed tests
+class Status(Enum):
+    READY = auto()
+    COMP_FAIL = auto()
+    COMP_TIMEOUT = auto()
+    EXEC_FAIL = auto()
+    EXEC_TIMEOUT = auto()
+    SUCCESS = auto()
 
-def run_test(filepath, expected_result):
-    global PASS_COUNT, FAIL_COUNT
+@dataclass
+class Test:
+    status: Status
+    name: str
+    files: list[Path]
+    comp_timeout: timedelta = timedelta(seconds=2)
+    exec_timeout: timedelta = timedelta(seconds=2)
+    comp_stdout: str = ""
+    comp_stderr: str = ""
+    exec_stdout: str = ""
+    exec_stderr: str = ""
 
-    print(f"{filepath}: ", end=" ")
+def discover_tests(root: Path) -> list[Test]:
+    tests = []
 
-    src_files = []
+    for file in root.glob("*.frx"):
+        tests.append(
+            Test(
+                status = Status.READY,
+                name = file.stem,
+                files = [file]
+            )
+        )
 
-    if os.path.isfile(filepath):
-        src_files.append(filepath)
-    elif os.path.isdir(filepath):
-        for root, _, filenames in os.walk(filepath):
-            for name in filenames:
-                src_files.append(os.path.join(root, name))
-    else:
-        print(Fore.YELLOW + "INVALID" + Style.RESET_ALL)
-        FAIL_COUNT += 1
-        return
+    for dirpath in root.iterdir():
+        if not dirpath.is_dir():
+            continue
 
-    with tempfile.NamedTemporaryFile(delete=False) as temp_executable:
-        temp_executable_name = temp_executable.name
+        entry = dirpath / "main.frx"
+        if not entry.exists():
+            continue
 
-    # TODO: Use the below line as soon as the compiler supports the -o flag
-    # compile_result = subprocess.run([COMPILER, "-o", temp_executable.name, filepath], capture_output=True, text=True)
-    compile_result = subprocess.run([COMPILER] + src_files, capture_output=True, text=True)
-    actual_result = compile_result.returncode
-    compile_output = compile_result.stdout + compile_result.stderr
+        files = sorted(
+            f for f in dirpath.rglob("*.frx")
+            if f != entry
+        )
 
-    if actual_result == expected_result:
-        if expected_result == 0:
-            # TODO: Use the below line when we no longer generate the executable in a local ./a.out file
-            #exec_result = subprocess.run([temp_executable.name], capture_output=True, text=True)
-            exec_result = subprocess.run(["./a.out"], capture_output=True, text=True)
-            exec_returncode = exec_result.returncode
-            exec_output = exec_result.stdout + exec_result.stderr
+        tests.append(
+            Test(
+                status = Status.READY,
+                name = dirpath.name,
+                files = [entry, *files]
+            )
+        )
 
-            if exec_returncode == 0:
-                print(Fore.GREEN + "PASS" + Style.RESET_ALL)
-                PASS_COUNT += 1
-            else:
-                print(Fore.RED + "FAIL (execution)" + Style.RESET_ALL)
-                print(exec_output)
-                FAIL_COUNT += 1
+        print([entry, files])
+
+    tests = sorted(tests, key = lambda t: t.name)
+
+    global MAX_TEST_NAME_LEN
+    MAX_TEST_NAME_LEN = max((len(t.name) for t in tests), default=0) + 2
+
+    return tests
+
+def run_tests(tests: list[Test]) -> bool:
+    every_test_passed = True
+
+    for test in tests:
+        output_file = tempfile.NamedTemporaryFile(delete = False)
+        output_file.close()
+        output_path = Path(output_file.name)
+
+        compile_cmd = [COMPILER] + [str(f) for f in test.files] + ["-o", str(output_path)]
+
+        try:
+            compile_proc = subprocess.run(
+                compile_cmd,
+                capture_output = True,
+                text = True,
+                timeout = test.comp_timeout.total_seconds()
+            )
+        except subprocess.TimeoutExpired:
+            test.status = Status.COMP_TIMEOUT
+            test.comp_stdout = compile_proc.stdout
+            test.comp_stderr = compile_proc.stderr
+            output_path.unlink(missing_ok=True)
+            every_test_passed = False
+            print_test(test)
+            continue
+
+        test.comp_stdout = compile_proc.stdout
+        test.comp_stderr = compile_proc.stderr
+
+        if compile_proc.returncode != 0:
+            test.status = Status.COMP_FAIL
+            output_path.unlink(missing_ok=True)
+            every_test_passed = False
+            print_test(test)
+            continue
+
+        try:
+            exec_proc = subprocess.run(
+                [str(output_path)],
+                capture_output = True,
+                text = True,
+                timeout=test.exec_timeout.total_seconds()
+            )
+        except subprocess.TimeoutExpired:
+            test.status = Status.EXEC_TIMEOUT
+            test.exec_stdout = exec_proc.stdout
+            test.exec_stderr = exec_proc.stderr
+            output_path.unlink(missing_ok=True)
+            every_test_passed = False
+            print_test(test)
+            continue
+
+        test.exec_stdout = exec_proc.stdout
+        test.exec_stderr = exec_proc.stderr
+
+        if exec_proc.returncode == 0:
+            test.status = Status.SUCCESS
         else:
-            print(Fore.GREEN + "PASS" + Style.RESET_ALL)
-            PASS_COUNT += 1
-    else:
-        print(Fore.RED + "FAIL (compilation)" + Style.RESET_ALL)
-        print(compile_output)
-        FAIL_COUNT += 1
+            test.status = Status.EXEC_FAIL
+            every_test_passed = False
 
-    # TODO: Use the below lines when we no longer generate the executable in a local ./a.out file
-    # if os.path.exists(temp_executable_name):
-    #     os.remove(temp_executable_name)
-    if os.path.exists("./a.out"):
-        os.remove("./a.out")
+        print_test(test)
+        output_path.unlink(missing_ok=True)
+
+    return every_test_passed
+
+def print_test(test: Test):
+        match test.status:
+            case Status.COMP_FAIL: status_str = f"{Fore.RED + Style.BRIGHT}FAIL (compilation){Style.RESET_ALL}"
+            case Status.COMP_TIMEOUT: status_str = f"{Fore.RED + Style.BRIGHT}FAIL (compilation timeout){Style.RESET_ALL}"
+            case Status.EXEC_FAIL: status_str = f"{Fore.RED + Style.BRIGHT}FAIL (execution){Style.RESET_ALL}"
+            case Status.EXEC_TIMEOUT: status_str = f"{Fore.RED + Style.BRIGHT}FAIL (execution timeout){Style.RESET_ALL}"
+            case Status.SUCCESS: status_str = f"{Fore.GREEN}PASS{Style.RESET_ALL}"
+
+        spaces = " " * (MAX_TEST_NAME_LEN - (len(test.name) + 1))
+        print(f"{test.name}:{spaces}{status_str}")
+
+def print_tests_summary(tests: list[Test], verbose = False):
+    tests_passed = 0
+    tests_failed = 0
+
+    summary_len = len(f"{len(tests)} total | {tests_passed} passed | {tests_failed} failed")
+
+    for test in tests:
+        if test.status == Status.SUCCESS:
+            tests_passed += 1
+        else:
+            tests_failed += 1
+
+    if verbose:
+        line = "Failed tests output:"
+        print("=" * summary_len)
+        print(line + '\n')
+        for test in tests:
+            if test.status == Status.SUCCESS:
+                continue
+
+            print(f"{test.name}:")
+
+            if test.comp_stdout:
+                print(f"compilation (stdout):\n{test.comp_stdout}")
+
+            if test.comp_stderr:
+                print(f"compilation (stderr):\n{test.comp_stderr}")
+
+            if test.exec_stdout:
+                print(f"execution (stdout):\n{test.exec_stdout}")
+
+            if test.exec_stderr:
+                print(f"execution (stderr):\n{test.exec_stderr}")
+
+            print("=" * summary_len)
+
+    if not verbose:
+        print("=" * summary_len)
+
+    print(f"{len(tests)} total | {Fore.GREEN}{tests_passed} passed{Style.RESET_ALL} | {Fore.RED + Style.BRIGHT}{tests_failed} failed{Style.RESET_ALL}")
+    print("=" * summary_len)
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog = "./tests.py",
+        description = "Test runner for the Furox compiler"
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action = "store_true",
+        help = "enable showing compiler and execution output for failed tests"
+    )
+
+    return parser.parse_args()
 
 def main():
-    global PASS_COUNT, FAIL_COUNT
-    valid_tests = [os.path.join(TEST_DIR, "pass", f) for f in os.listdir(os.path.join(TEST_DIR, "pass"))]
-    invalid_tests = [os.path.join(TEST_DIR, "fail", f) for f in os.listdir(os.path.join(TEST_DIR, "fail"))]
+    init() # colorama initialization
 
-    total_tests = len(valid_tests) + len(invalid_tests)
+    args = parse_args()
 
-    for test in valid_tests:
-        run_test(test, 0)
+    status = 0
+    tests = discover_tests(Path(TESTS_DIR))
+    if not run_tests(tests):
+        status = 1
+    print_tests_summary(tests, verbose = args.verbose)
 
-    for test in invalid_tests:
-        run_test(test, 1)
-
-    if PASS_COUNT == total_tests:
-        print(Fore.GREEN + f"All ({total_tests}) tests passed" + Style.RESET_ALL)
-    else:
-        print(Fore.RED + f"{PASS_COUNT}/{total_tests} tests passed" + Style.RESET_ALL)
-
-    exit(FAIL_COUNT)
+    return status
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
