811493bc33d00309e78a814ce6e7f48e7def18c2
Noxmor <lord.noxmor@gmail.com>
2026-04-21 20:46:39 +0200

    Fix type resolution of unary expressions for address of and deref operators

--- bootstrap/src/unary_expr.c
+++ bootstrap/src/unary_expr.c
@@ -1,6 +1,7 @@
 #include "assert.h"
 #include "ast.h"
 #include "attributes_table.h"
+#include "operator.h"
 #include "resolution.h"
 #include "sema.h"
 
@@ -17,6 +18,22 @@ void unary_expr_resolve(AST* ast, ResolutionContext* ctx)
     ast_resolve(unary_expr->operand, ctx);
 
     const Type* type = expr_infer_type(unary_expr->operand);
+
+    if (unary_expr->operator == FRX_OPERATOR_ADDRESS_OF)
+    {
+        type = type_intern_ptr(type, FRX_TRUE);
+    }
+    else if (unary_expr->operator == FRX_OPERATOR_DEREF
+        && type->kind == FRX_TYPE_KIND_PTR)
+    {
+        type = type->ptr.base;
+    }
+    else if (unary_expr->operator == FRX_OPERATOR_DEREF
+        && type->kind == FRX_TYPE_KIND_ARRAY)
+    {
+        type = type->array.base;
+    }
+
     attributes_table_insert_type(ast->id, type);
 }
 
